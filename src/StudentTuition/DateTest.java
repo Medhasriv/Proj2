84ca16ac6f8cc1f0b03f5ba8769b204bc26ab784
Shreya Pandey <42945783+Xhrya@users.noreply.github.com>
2023-02-21 17:12:11 -0500

    test classes

---
+++ src/StudentTuition/DateTest.java
@@ -0,0 +1,11 @@
+package StudentTuition;
+
+import static org.junit.Assert.*;
+
+public class DateTest {
+
+    @org.junit.Test
+    public void isValid() {
+        //shreya will do
+    }
+}
---
+++ src/StudentTuition/InternationalTest.java
@@ -0,0 +1,12 @@
+package StudentTuition;
+
+import static org.junit.Assert.*;
+
+public class InternationalTest {
+
+    @org.junit.Test
+    public void tuitionDue() {
+        int a;
+        //shreya will do
+    }
+}
