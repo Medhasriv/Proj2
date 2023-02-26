f658d169b450851e9a16db6d239fc6c0e32dd450
Shreya Pandey <42945783+Xhrya@users.noreply.github.com>
2023-02-25 21:08:54 -0500

    fixed some compilation errors

--- src/StudentTuition/InternationalTest.java
+++ src/StudentTuition/InternationalTest.java
@@ -6,14 +6,14 @@ public class InternationalTest {
 
     @org.junit.Test
     public void tuitionDue_isStudyAbroad() {
-        Date d = new Date("10/10/s2003")
+        Date d = new Date("10/10/2003");
         Profile p = new Profile("SantiClaus", "J", d);
         International i = new International(p, Major.CS,12, false);
         assertEquals(5918, i.tuitionDue(i.returnCredits()));
     }
 
     public void tuitionDue_isNotStudyAbroad() {
-        Date d = new Date("10/10/2003")
+        Date d = new Date("10/10/2003");
         Profile p = new Profile("SantiClaus", "J", d);
         International i = new International(p, Major.CS,12, true);
         assertEquals(35655, i.tuitionDue(i.returnCredits()));
